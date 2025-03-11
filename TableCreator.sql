b861ee8bb5daae46e5fabf90852728a911df1851
DdroidES <nicolascesti@gmail.com>
2025-03-11 19:13:02 +0100

    first commit

--- TableCreator.sql
+++ TableCreator.sql
@@ -1,22 +1,23 @@
-CREATE TABLE Humidity ( ,
-    timestamp VARCHAR(255) PRIMARY KEY,
+CREATE TABLE Humidity (
+    id SERIAL PRIMARY KEY
+    timestamp VARCHAR(255) ,
     value FLOAT
 );
 
 CREATE TABLE Irrigation (
     id SERIAL PRIMARY KEY ,
-    timestamp VARCHAR(255) REFERENCES Humidity(timestamp),
+    timestamp VARCHAR(255) ,
     value  FLOAT
 );
 
 CREATE TABLE RainGauge (
     id SERIAL PRIMARY KEY ,
-    timestamp VARCHAR(255) REFERENCES Humidity(timestamp),
+    timestamp VARCHAR(255) ,
     value  FLOAT
 );
 
 CREATE TABLE Temperature (
     id SERIAL PRIMARY KEY ,
-    timestamp VARCHAR(255) REFERENCES Humidity(timestamp),
+    timestamp VARCHAR(255) ,
     value  FLOAT
 );
